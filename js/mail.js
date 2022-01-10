4ed452886637477f2fa77c4b4a6e6f66d884c64c
iram96 <mariano.maselli@virgilio.it>
2022-01-10 13:50:01 +0100

    Added first files

---
+++ js/mail.js
@@ -0,0 +1,4 @@
+/* Crea un array di email;
+Chiedi all’utente la sua email.
+controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.
+*/
